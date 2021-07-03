89d7e251cf62a790d2f5f85afbce6bcbb24f8ec8
Zola <lazar.s.lukic@gmail.com>
2021-07-04 01:27:58 +0200

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,25 @@
  */
 
 module.exports = {
-  /* Your site config here */
   pathPrefix: "/showcase",
+  siteMetadata: {
+    title: `Zola - Showcase`,
+    description: `Showcase for personal projects`,
+    author: `@zolalsl`,
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `experiment-g-default`,
+        short_name: `expg`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#0054a6`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
